7176c6847497a95a626f10241f1dcf08d9cd7d7b
phuduy2610 <phuduy2610@gmail.com>
2020-12-08 02:59:29 +0700

    Upload ảnh local warning: sẽ thêm cloud vào sáng mai

--- controllers/editproductController.js
+++ controllers/editproductController.js
@@ -1,14 +1,38 @@
+const formidable = require('formidable');
+const fs = require('fs');
+
 const GameModel = require('../models/GameModel');
 
 //updateGameByName("Dragon Age II", { Price: 30$, genre: FPS });
-exports.editproduct = async(req,res,next)=>{
-    console.log("hereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-    console.log(req.body.name);
-    console.log(req.body.cover);
-    console.log(req.body.basePrice);
-    console.log(req.body.description);
-    await GameModel.updateGameByName(req.body.name,{ basePrice: req.body.basePrice, cover: req.body.cover, title: req.body.name, Description: req.body.description});
-    res.redirect('/product');
+exports.editproduct = (req,res,next)=>{
+    const form = formidable({ multiples: true });
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+          next(err);
+          return;
+        }
+        const coverImage =files.coverImage;
+        if(coverImage && coverImage.size > 0){
+            const imageName =fields.name.replace(/\s+/g, '') + "coverImg." + coverImage.name.split('.').pop() ;
+            //TODO IMPORTANT: Sửa khi quăng lên cloud 
+            fs.copyFileSync(coverImage.path, __dirname +'/../public/images/' + imageName  );
+            fs.copyFileSync(coverImage.path,'/Study/LTW/DOAN/public/images/' + imageName  );
+            fields.cover = '/images/'+ imageName;
+        }
+        
+        GameModel.updateGameByName(fields.name,
+            { basePrice:fields.basePrice, cover: fields.cover, title: fields.name, Description: fields.description})
+            .then(()=>{
+                res.redirect('/product');
+            });
+    });
+
+    // console.log(req.body.name);
+    // console.log(req.body.cover);
+    // console.log(req.body.basePrice);
+    // console.log(req.body.description);
+    // await GameModel.updateGameByName(req.body.name,{ basePrice: req.body.basePrice, cover: req.body.cover, title: req.body.name, Description: req.body.description});
+    // res.redirect('/product');
 }
 
 exports.edit = async (req,res,next) => {
